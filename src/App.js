92b92b20844415226503df7991c888835252d2d5
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-27 21:52:09 -0400

    Construct the structure of App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import About from "./components/About/index";
-
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
-    return(
-        <About />
-    );
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Resume></Resume>
+        <Contact></Contact>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 }
-
 
 export default App;
