d83d3a5b498cd7cb7f7a60e96368b7813c5e7561
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-30 22:22:53 -0400

    Add download cv button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
     return (
@@ -7,6 +8,7 @@ const Header = () => {
             <h5>Hello I'm</h5>
             <h1>Chen Wang</h1>
             <h5 className='text-align'> Full Stack Data Scientist</h5>
+            <CTA />
         </header>
     )
 }
