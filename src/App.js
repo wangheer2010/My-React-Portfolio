af7429f64aead06e3db88cc8c4179dea6363bd5d
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-30 23:19:00 -0400

    Add nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import NavTabs from './components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
-import Resume from './components/Resume/Resume';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div>
       <Header></Header>
-      <NavTabs></NavTabs>
+      <Nav></Nav>
       <About></About>
       <Portfolio></Portfolio>
-      <Resume></Resume>
       <Contact></Contact>
       <Footer></Footer>
     </div>
